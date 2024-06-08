e5a98be96839f9270c4873c3c76749a6982e0fff
6dc <ceo@splex.net>
2024-06-08 21:00:22 +0200

    update parameter

--- lib/src/models/reel_model.dart
+++ lib/src/models/reel_model.dart
@@ -3,13 +3,15 @@ import 'package:spacity_wheels_viewer/spacity_wheels_viewer.dart';
 class ReelModel {
   final String? id;
   final String url;
-  bool isLiked=false;
-  bool isFollowed=false;
+  bool isLiked;
+  bool isFollowed;
   final int likeCount;
+  final bool? showFollowButton;
+  final bool? s;
   final String userName;
   final String? profileUrl;
   final String? reelDescription;
   final String? musicName;
   final List<ReelCommentModel>? commentList;
-  ReelModel(this.url,this.userName, this.isLiked,this.isFollowed,{this.id,this.likeCount=0,this.profileUrl,this.reelDescription,this.musicName,this.commentList});
+  ReelModel(this.url,this.userName, this.isLiked,this.isFollowed, this.s,{this.id,this.showFollowButton,this.likeCount=0,this.profileUrl,this.reelDescription,this.musicName,this.commentList});
 }
